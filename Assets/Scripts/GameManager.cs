523aa03b5d10ff76cd91f8706293e7417cf61f7c
Dominik Mryka <dominik.mryka@alumni.fh-aachen.de>
2021-04-26 17:16:21 +0200

    fixed some things, rewrited scripts, deleted unnecessary scripts, cleaned up project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool GameIsOver;
+
+	[SerializeField] private HUD hud;
+
+	// TODO: no effect currently
+	public static bool makeItJuicy = true;
+
+
+    void Start()
+    {
+		hud = GetComponent<HUD>();
+        GameIsOver = false;
+    }
+	void Update()
+	{
+		hud.SetPointCounter(PlayerStats.currScore);
+		hud.SetHealthAmount(PlayerStats.currHealth);
+		if (Input.GetKey(KeyCode.J)) makeItJuicy = !makeItJuicy;
+		if (GameIsOver)
+			return;
+
+		if (PlayerStats.currHealth <= 0)
+		{
+			EndGame();
+		}
+	}
+
+	void EndGame()
+    {
+		Debug.Log("EndGame");
+    }
+
+	public void WinLevel()
+	{
+		Debug.Log("WinLevel");
+	}
+}
