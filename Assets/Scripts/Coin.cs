579f09053c7691d93d07673ff0c6c81e7b7e979c
Dominik Mryka <dominik.mryka@alumni.fh-aachen.de>
2021-04-28 11:33:22 +0200

    reimported PlayerStats and Coin.cs

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private int points;
+
+    //[SerializeField] private GameObject collectEffect;
+
+    [SerializeField] private AudioClip collectSound;
+
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerStats>().IncreaseScore(points);
+            //GameObject effect = (GameObject)Instantiate(collectEffect, transform.position, Quaternion.identity);
+            //Destroy(effect, 5f);
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+            Destroy(this.gameObject);
+        }
+    }
+}
